293afe20d909eedf1f6ee43bb18bda655fbb1d6e
Oswin Jerome <oswinjeromej@gmail.com>
2020-03-25 13:17:01 +0530

    Updates

---
+++ precache-manifest.98ea14420984796c3862a52c70c092c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b354bb4f898afa1cfe6968e79a9979ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "890ec7f275a86fa76b23",
+    "url": "/static/css/main.809976c2.chunk.css"
+  },
+  {
+    "revision": "33fe552eeb0c5e60e943",
+    "url": "/static/js/2.d2a855ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2a855ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890ec7f275a86fa76b23",
+    "url": "/static/js/main.b369468c.chunk.js"
+  },
+  {
+    "revision": "9d4a124a4513fc35f7b1",
+    "url": "/static/js/runtime-main.8143f3e2.js"
+  }
+]);
