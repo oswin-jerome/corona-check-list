ac15d533c0dd585a2c26b62136559bbf8500c8c0
Oswin Jerome <oswinjeromej@gmail.com>
2020-03-25 13:26:57 +0530

    Updates

---
+++ precache-manifest.f82b75769097a55112899d1c38718ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fef65da7e122887cac1ffec87625db",
+    "url": "/corona-check-list/index.html"
+  },
+  {
+    "revision": "3aaa42728e72d495b72a",
+    "url": "/corona-check-list/static/css/main.809976c2.chunk.css"
+  },
+  {
+    "revision": "33fe552eeb0c5e60e943",
+    "url": "/corona-check-list/static/js/2.d2a855ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-check-list/static/js/2.d2a855ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aaa42728e72d495b72a",
+    "url": "/corona-check-list/static/js/main.76b9ccd9.chunk.js"
+  },
+  {
+    "revision": "519ecf29fdf8d672f0f9",
+    "url": "/corona-check-list/static/js/runtime-main.e7db34d6.js"
+  }
+]);
